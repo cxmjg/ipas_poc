4f64deca066738e4d7faef1e177f34c117853b54
Marcos Garcia <mgarciaunlp@gmail.com>
2025-06-02 11:18:28 -0300

    completar metodos de creacion, administracion y refactor a clase

--- Backend/DockerDriver.py
+++ Backend/DockerDriver.py
@@ -1,27 +1,60 @@
 import docker
 
-client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
+class DockerDriver:
+    def __init__(self, base_url='tcp://127.0.0.1:2375'):
+        self.client = docker.DockerClient(base_url=base_url)
 
-def descargarImagen(repositorio, tag='latest'):
-    imagenes = client.images.list()
-    if repositorio not in imagenes:
-        imagen = client.images.pull(repositorio, tag)
-    return imagen
+    def descargar_imagen(self, repositorio, tag='latest'):
+        print('Buscando imagen...')
+        imagenes = self.client.images.list(name=repositorio)
+        if len(imagenes) == 0:
+            print('Imagen no existe, iniciando proceso de descarga...')
+            imagen = self.client.images.pull(repositorio, tag)
+        else:
+            print('Imagen existe')
+            imagen = self.client.images.get(repositorio)
+        print('Finalizado')
+        return imagen
 
-def listarContenedores():
-    for contenedor in client.containers.list(all=True):
-        print(contenedor.name)
+    def listar_contenedores(self):
+       return self.client.containers.list(all=True)
 
-def eliminarContenedores():
-    for contenedor in client.containers.list(all=True):
-        contenedor.remove()
+    def eliminar_contenedores(self):
+        for contenedor in self.client.containers.list(all=True):
+            contenedor.remove()
 
-def crearContenedor(imagen, nombre, comandos=[]):
-    client.containers.create(image=imagen, command=comandos, name=nombre)
+    def crear_contenedor(self, imagen, nombre, comandos=None, env=None, puertos=None):
+        if comandos is None:
+            comandos = []
+        if env is None:
+            env = {}
+        if puertos is None:
+            puertos = {}
+        return self.client.containers.create(image=imagen, command=comandos, name=nombre, ports=puertos, environment=env)
 
-def ejecutarContenedor(nombre):
-    client.containers.run(nombre)
+    def ejecutar_contenedor(self, nombre):
+        contenedor = self.client.containers.get(nombre)
+        contenedor.start()
+        return contenedor.status
 
+    def detener_contenedor(self, nombre):
+        contenedor = self.client.containers.get(nombre)
+        contenedor.stop()
+        return contenedor.status
+    
+    
+    
 
-crearContenedor(descargarImagen('lscr.io/linuxserver/vscodium'), 'vscodium')
-listarContenedores()
+webtopEnv = {
+
+}
+
+webtopPorts = {
+    '3000/tcp': 3000,
+    '3001/tcp': 3001
+}
+
+crearContenedor(descargarImagen('lscr.io/linuxserver/webtop'), 'webtop', puertos=webtopPorts)
+detenerContenedor('webtop')
+ejecutarContenedor('webtop')
+#listarContenedores()
