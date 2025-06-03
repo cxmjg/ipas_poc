64c9b91e2d7c85652633ca19d259a305c9271c1d
cxmjg <mgarciaunlp@gmail.com>
2025-06-03 20:00:49 -0300

    se creo la pantalla de mis contenedores

--- Frontend/Componentes/MenuLateral.py
+++ Frontend/Componentes/MenuLateral.py
@@ -2,11 +2,12 @@ from nicegui import ui
 
 class MenuLateral:
     def __init__(self, opciones, usuario):
+        self.botones = {}
         with ui.left_drawer(value=False).classes('bg-blue-300 items-center justify-evenly z-40').props('overlay') as self.menuLateral:
             
             with ui.column() as contenedorBotones:
                 for opcion in opciones.keys():
-                    ui.button(opcion, on_click= lambda: opciones[opcion]()).classes('w-[180px] h-auto text-center items-center')
+                    self.botones[opcion] = ui.button(opcion, on_click= lambda: opciones[opcion]()).classes('w-[180px] h-auto text-center items-center')
 
             ui.separator()
 
@@ -19,6 +20,11 @@ class MenuLateral:
 
             with ui.column() as contenedorBoton:
                 ui.button('Cerrar Sesión').classes('w-[180px] h-auto text-center items-center text-black')
+                
+
+    def setOpciones(self, opcion):
+        self.botones[opcion['nombre']].on_click(opcion['accion'])
+        
 
     def getMenuLateral(self):
         return self.menuLateral
--- Frontend/Index.py
+++ Frontend/Index.py
@@ -14,22 +14,58 @@ usuario = {
 }
 
 opcionesMenu = {
-    'Mis Documentos':mostrarAlerta,
-    'Mis Contenedores': mostrarAlerta
+    'Mis Documentos':None,
+    'Mis Contenedores': None
 }
 
-menuLateral = MenuLateral(opcionesMenu, usuario).getMenuLateral()
-header = Header(menuLateral)
+columns = [
+    {'name': 'nombre', 'label': 'Nombre', 'field': 'nombre', 'required': True, 'align': 'left'},
+    {'name': 'imagen', 'label': 'Imagen', 'field': 'imagen', 'sortable': True},
+    {'name': 'estado', 'label': 'Estado', 'field': 'estado', 'sortable': True},
+    {'name': 'puertos', 'label': 'Puertos', 'field': 'puertos', 'sortable': True},
+    {'name': 'link', 'label': 'Link', 'field': 'link', 'sortable': True},
+]
+rows = [
+    {'nombre': 'Escritorio XFCE', 'imagen': 'webtop', 'estado':'Activo', 'puertos': '', 'link':'http://localhost:3000'},
+    {'nombre': 'Base MySQL', 'imagen': 'mysql', 'estado':'Detenido', 'puertos': '', 'link':''},
+    {'nombre': 'Servidor WEB', 'imagen': 'apache', 'estado':'Detenido', 'puertos': '', 'link':''},
+]
+
+
+menuLateral = MenuLateral(opcionesMenu, usuario)
+header = Header(menuLateral.getMenuLateral())
 header.getHeader()
 
-with ui.row().classes('items-center justify-center p-0 m-0 gap-0 flex-col sm:flex-row ') as main:
-    ui.label('Empieza a Programar Rápido, Desde Cualquier Lugar').classes('text-center sm:text-left text-3xl lg:text-7xl md:text-5xl w-[50%] font-extrabold font-sans')
-    ui.image('./Frontend/Images/Logo.png').classes('w-[35%] p-0 m-0')
+with html.section().classes('w-full justify-evenly') as main:
+    
+    with ui.row().classes('items-center justify-center p-0 m-0 gap-0 flex-col sm:flex-row ') as home:
+        ui.label('Empieza a Programar Rápido, Desde Cualquier Lugar').classes('text-center sm:text-left text-3xl lg:text-7xl md:text-5xl w-[50%] font-extrabold font-sans')
+        ui.image('./Frontend/Images/Logo.png').classes('w-[35%] p-0 m-0')
+    
+    with ui.column().classes('w-full justify-evenly items-center') as misContenedores:
+        contenedores = ui.table(columns=columns, rows=rows, row_key='nombre').classes('text-black')
+        contenedores.add_slot('body-cell-estado', '''
+            <q-td :props="props">
+                <q-badge :color="props.value == 'Detenido' ? 'red' : 'green'">
+                    {{ props.value }}
+                </q-badge>
+            </q-td>
+        ''')
+        contenedores.add_slot('body-cell-link', '''
+            <q-td :props="props">
+                <a :href="props.value" target="_blank" v-if="props.value">ABRIR</a>
+            </q-td>
+        ''')
+    
 
-
+misContenedores.set_visibility(visible=False)
 footer = Footer()
 footer.getFooter()
 ui.timer(1.0, footer.actualizarFechaHora)
 
+def mostrarContenedores():
+    home.set_visibility(visible=False)
+    misContenedores.set_visibility(visible=True)
 
+menuLateral.setOpciones({'nombre': 'Mis Contenedores', 'accion': mostrarContenedores})
 ui.run()
