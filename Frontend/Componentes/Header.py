438465d625f23d440419c7c0f8cd9b5d6183f36b
RamiroSabetta <155183794+RamiroSabetta@users.noreply.github.com>
2025-06-03 20:49:04 -0300

    Redireccion a home

--- Frontend/Componentes/Header.py
+++ Frontend/Componentes/Header.py
@@ -1,12 +1,18 @@
 from nicegui import ui, html
 
 class Header:
-    def __init__(self, menuLateral_):
+    def __init__(self, menuLateral_, home):
+        self.home = home
         with ui.header().classes('items-center p-0') as self.header:
             ui.button(on_click=menuLateral_.toggle, icon='menu').props('flat color=white')
             with ui.row().classes('items-center'):
-                html.h1('IPAS').classes('text-5xl font-extrabold font-sans')
+                with ui.button(on_click=self.home).props('unelevated') as self.irHome:
+                    html.h1('IPAS').classes('text-5xl font-extrabold font-sans')
                 ui.html('<h2>Infraestructura Plataforma Ambiente Servicio</h2>').classes('text-lg text-gray-600 italic')
 
     def getHeader(self):
         return self.header
+    
+    def setGoToHome(self, home):
+        self.home=home
+        self.irHome.on_click(home)
--- Frontend/Index.py
+++ Frontend/Index.py
@@ -31,15 +31,15 @@ rows = [
     {'nombre': 'Servidor WEB', 'imagen': 'apache', 'estado':'Detenido', 'puertos': '', 'link':''},
 ]
 
-
 menuLateral = MenuLateral(opcionesMenu, usuario)
-header = Header(menuLateral.getMenuLateral())
+header = Header(menuLateral.getMenuLateral(), None)
 header.getHeader()
 
 with html.section().classes('w-full justify-evenly') as main:
     
     with ui.row().classes('items-center justify-center p-0 m-0 gap-0 flex-col sm:flex-row ') as home:
         ui.label('Empieza a Programar Rápido, Desde Cualquier Lugar').classes('text-center sm:text-left text-3xl lg:text-7xl md:text-5xl w-[50%] font-extrabold font-sans')
+        
         ui.image('./Frontend/Images/Logo.png').classes('w-[35%] p-0 m-0')
     
     with ui.column().classes('w-full justify-evenly items-center') as misContenedores:
@@ -63,9 +63,17 @@ footer = Footer()
 footer.getFooter()
 ui.timer(1.0, footer.actualizarFechaHora)
 
+def goToHome():
+    home.set_visibility(visible=True)
+    misContenedores.set_visibility(visible=False)
+    menuLateral.getMenuLateral().hide()
+
 def mostrarContenedores():
     home.set_visibility(visible=False)
     misContenedores.set_visibility(visible=True)
 
 menuLateral.setOpciones({'nombre': 'Mis Contenedores', 'accion': mostrarContenedores})
+
+header.setGoToHome(goToHome)
+
 ui.run()
