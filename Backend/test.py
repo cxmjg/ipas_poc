40f53dec1b20673ffd5bee3825d5cc02393e2033
Marcos Garcia <mgarciaunlp@gmail.com>
2025-06-04 09:46:46 -0300

    diccionario contenedor

--- Backend/test.py
+++ Backend/test.py
@@ -2,24 +2,27 @@ from DockerDriver import DockerDriver
 
 docker = DockerDriver()
 
-webtopEnv = {}
-
-webtopPorts = {
-    '3000/tcp': 3000,
-    '3001/tcp': 3001
+contenedor = {
+    'imagen':'lscr.io/linuxserver/webtop',
+    'nombre': 'webtop',
+    'puertos': {
+        '3000/tcp': 3000,
+        '3001/tcp': 3001
+    }
 }
 
-if docker.existeContenedor('webtop'):
-    docker.eliminarContenedor('webtop')
 
-docker.crearContenedor(docker.descargarImagen('lscr.io/linuxserver/webtop'), 'webtop', puertos=webtopPorts)
+if docker.existeContenedor(contenedor['nombre']):
+    docker.eliminarContenedor(contenedor['nombre'])
 
-print(docker.detenerContenedor('webtop'))
+docker.crearContenedor(docker.descargarImagen(contenedor['imagen']), contenedor['nombre'], puertos=contenedor['puertos'])
 
-print(docker.ejecutarContenedor('webtop'))
+print(docker.detenerContenedor(contenedor['nombre']))
 
-print(docker.reiniciarContenedor('webtop'))
+print(docker.ejecutarContenedor(contenedor['nombre']))
 
-print(docker.getSalud('webtop'))
+print(docker.reiniciarContenedor(contenedor['nombre']))
+
+print(docker.getSalud(contenedor['nombre']))
 
 print('El contenedor puede ser accedido utilizando la siguiente url: http://localhost:3000')
